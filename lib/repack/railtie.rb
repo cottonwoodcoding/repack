19ec77264463b3985cd7dd41047aa0c0cab3961d
eatspaint <cef2010@gmail.com>
2016-11-10 17:41:09 -0700

    Replaces rake webpack:compile, with repack:compile

--- lib/repack/railtie.rb
+++ lib/repack/railtie.rb
@@ -12,14 +12,14 @@ module Repack
     end
 
     config.repack = ActiveSupport::OrderedOptions.new
-    config.repack.config_file = 'config/repack.config.js'
-    config.repack.binary = 'node_modules/.bin/repack'
+    config.repack.config_file = 'config/webpack.config.js'
+    config.repack.binary = 'node_modules/.bin/webpack'
 
     config.repack.dev_server = ActiveSupport::OrderedOptions.new
     config.repack.dev_server.host = 'localhost'
     config.repack.dev_server.port = 3808
     config.repack.dev_server.https = false # note - this will use OpenSSL::SSL::VERIFY_NONE
-    config.repack.dev_server.binary = 'node_modules/.bin/repack-dev-server'
+    config.repack.dev_server.binary = 'node_modules/.bin/webpack-dev-server'
     config.repack.dev_server.enabled = !::Rails.env.production?
 
     config.repack.output_dir = "public/client"
@@ -27,7 +27,7 @@ module Repack
     config.repack.manifest_filename = "manifest.json"
 
     rake_tasks do
-      load "tasks/webpack.rake"
+      load "tasks/repack.rake"
     end
   end
 end

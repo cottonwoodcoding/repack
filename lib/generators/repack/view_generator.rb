0c6bda5f75e5e1d65abd323e937c140cf6f45418
eatspaint <cef2010@gmail.com>
2017-03-27 21:31:11 -0600

    Improves view generator matching for: - Controller names - MultipleWordNames

--- lib/generators/repack/view_generator.rb
+++ lib/generators/repack/view_generator.rb
@@ -10,8 +10,9 @@ module Repack
       @view = args[0]
     end
 
+    # ASSUMPTION: entry file will be snake_cased
     def update_webpack_entry
-      name = @view.downcase.gsub(/ /, "_")
+      name = @view.underscore
       path = "'#{name}': './client/#{name}.js',"
       insert_into_file 'config/webpack.config.js', after: /entry: {\n/ do
         <<-CONFIG
@@ -21,12 +22,13 @@ module Repack
     end
 
     def create_entry_file
-      file = "client/#{@view.gsub(/ /, '')}.js"
+      file = "client/#{@view.underscore}.js"
       name = @view.titleize.gsub(/ /, '')
       copy_file "boilerplate/views/ViewTemplate.js", file
       gsub_file file, /Placeholder/, name
     end
 
+    # ASSUMPTION: container will be PascalCased
     def create_container
       name = @view.titleize.gsub(/ /, '')
       file = "client/containers/#{name}.js"
@@ -34,15 +36,19 @@ module Repack
       gsub_file file, /Placeholder/, name
     end
 
+    # ASSUMPTION: Rails controllers will be PascalCased, file & directory names will be snake_cased
     def create_rails_view
-      name = @view.downcase.gsub(/ /, '_')
-      empty_directory "app/views/#{name.pluralize}"
+      name = @view.underscore
+      Rails.application.eager_load! if ApplicationController.descendants.length == 0
+      controllers = ApplicationController.descendants.map { |cont| cont.name.gsub('Controller', '').underscore }
+      dirname = controllers.include?(name) ? name : name.pluralize
+      empty_directory "app/views/#{dirname}"
       if Gem.loaded_specs.has_key? 'haml-rails'
-        file = "app/views/#{name.pluralize}/index.html.haml"
+        file = "app/views/#{dirname}/index.html.haml"
         copy_file "boilerplate/views/rails_view.html.haml", file
         gsub_file file, /placeholder/, name
       else
-        file = "app/views/#{name.pluralize}/index.html.erb"
+        file = "app/views/#{dirname}/index.html.erb"
         copy_file "boilerplate/views/rails_view.html.erb", file
         gsub_file file, /placeholder/, name
       end
